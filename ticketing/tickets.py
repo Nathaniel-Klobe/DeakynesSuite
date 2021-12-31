eb89c4779e0b576d5046ee5ba39e9a68446fbc6a
Nathaniel-Klobe <nathaniel.klobe@gmail.com>
2021-12-30 21:17:25 -0600

    Trimming Tickets Down

--- ticketing/tickets.py
+++ ticketing/tickets.py
@@ -2,11 +2,13 @@ from datetime import date, datetime
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, url_for
 )
+from sqlalchemy.sql.expression import false
 
 from werkzeug.exceptions import abort
 
+from ticketing.database import db_session
 from ticketing.auth import login_required
-from ticketing.database import get_db
+from ticketing.models import Ticket
 
 bp = Blueprint('tickets', __name__, url_prefix='/tickets')
 
@@ -53,11 +55,7 @@ def create(id):
     if request.method == 'POST':
         ticket_type = request.form['ticket_type']
         ticket_description = request.form['ticket_description']
-        reference = request.form['reference']
         promised = request.form['promised']
-        created = date.today()
-        ticketstatus = 'New'
-        customer_id = id
         error = None
 
         if not ticket_type:
@@ -68,71 +66,27 @@ def create(id):
         if error is not None:
             flash(error)
         else:
-            db = get_db()
-            db.execute(
-                'INSERT INTO ticket (customer_id, ticket_type, ticket_description, reference, ticketstatus, created, promised) VALUES(?, ?, ?, ?, ?, ?, ?)',
-                (customer_id, ticket_type, ticket_description, reference, ticketstatus, created, promised) 
-            )
-            db.commit()
+            db_session.add(Ticket(id, ticket_type=ticket_type,
+                ticket_description=ticket_description, ticket_status='New', created=datetime.now(),
+                promised=promised, bDone=False, bNotified=False,
+                bRetrieved=False ))
+
+            db_session.commit()
             return redirect(url_for('tickets.main'))
 
     return render_template('app/ticket/create.html', id = id)
 
-    
-@bp.route('/create_customer', methods=('GET', 'POST'))
-@login_required
-def create_customer():
-    if request.method == 'POST':
-        first_name = request.form['first_name']
-        last_name = request.form['last_name']
-        street_address = request.form['street_address']
-        phone = request.form['phone']
-        
-        error = None
-
-        if not first_name:
-            error = 'First Name Required.'
-        elif not last_name:
-            error = 'Last Name Required.'
-        elif not street_address:
-            error = 'Address Required.'
-        elif not phone:
-            error = 'Phone Required.'
-
-        if error is not None:
-            flash(error)
-        else:
-            db = get_db()
-            result = db.execute(
-                'INSERT INTO customer (first_name, last_name, street_address, phone) VALUES(?, ?, ?, ?)',
-                (first_name, last_name, street_address, phone)
-            )
-            db.commit()
-            customer_id = result.lastrowid
-            return redirect(url_for('tickets.create', id = customer_id))
-    return render_template('app/customer/create.html')
-
 
 def get_ticket(id):
-    ticket = get_db().execute(
-        'SELECT t.id, c.last_name, c.phone, ticket_type, ticket_description, reference, ticketstatus, created, promised, sentoutlocation, sentoutdate, sentoutnotes, labor, parts, other, total, notes, completed, called, pickedup'
-        ' FROM ticket t JOIN customer c ON t.customer_id = c.id'
-        ' WHERE t.id = ?', (id,)
-    ).fetchone()
-
-    if ticket is None:
-        abort(404, f"Ticket id {id} doesn't exist.")
-
+    ticket = Ticket.query.filter(Ticket.id == 'id').first()
     return ticket
 
 
 @bp.route('/<int:id>/delete', methods=('POST',))
 @login_required
 def delete(id):
-    get_ticket(id)
-    db = get_db()
-    db.execute('DELETE FROM ticket WHERE id = ?', (id,))
-    db.commit()
+    ticket = get_ticket(id)
+    db_session.delete(ticket)
     return redirect(url_for('tickets.main'))
 
 
@@ -216,36 +170,6 @@ def complete(id):
             return redirect(url_for('tickets.main'))
 
     return render_template('app/ticket/complete.html', id = id, ticket = ticket)
-
-
-@bp.route('/<int:id>/sendout', methods=('GET', 'POST'))
-@login_required
-def sendout(id):
-    ticket = get_ticket(id)
-
-    if request.method == 'POST':
-        sentoutlocation = request.form['sentoutlocation']
-        sentoutnotes = request.form['sentoutnotes']
-        ticketstatus = 'sent out'
-        sentoutdate = date.today()
-        error = None
-
-        if sentoutlocation is None:
-            error = 'Sent Out Location Required'
-        
-        if error is not None:
-            flash(error)
-        else:
-            db = get_db()
-            db.execute(
-                'UPDATE ticket SET sentoutlocation = ?, sentoutnotes = ?, ticketstatus = ?, sentoutdate = ?'
-                ' WHERE id = ?',
-                (sentoutlocation, sentoutnotes, ticketstatus, sentoutdate, id)
-            )
-            db.commit()
-            return redirect(url_for('tickets.main'))
-
-    return render_template('app/ticket/sendout.html', id = id)
 
 
 @bp.route('/<int:id>/view')
